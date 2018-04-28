e837ac357296d8d57dba6eb05f0c9d740af8f5b4
Cleber Santos <oclebersantos@gmail.com>
2018-04-28 00:01:34 -0300

    teste 2

--- src/index.js
+++ src/index.js
@@ -16,3 +16,11 @@ element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 }
 
 document.body.appendChild(component());
+
+
+if (module.hot) {
+      module.hot.accept('./print.js', function() {
+             console.log('Accepting the updated printMe module!');
+            printMe();
+      })
+}
